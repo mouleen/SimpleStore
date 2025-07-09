951fd626f7dea837773567bb98d2a8dc60653fbf
mouleen <limauropro@gmail.com>
2025-07-09 18:27:27 +0000

    Login Form con estilos adaptados

--- src/front/components/Loginform.jsx
+++ src/front/components/Loginform.jsx
@@ -11,6 +11,7 @@ export const LoginForm = ({ setToken }) => {
   const [error, setError] = useState("");
   const [hasAuth,setHasAuth]= useState(false);
   const [loading, setLoading] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
   const {store, dispatch} =useGlobalReducer();
 
   const handleLogout = () => {
@@ -59,30 +60,40 @@ export const LoginForm = ({ setToken }) => {
 		}else{setHasAuth(false);}
   },[navigate,hasAuth])
   return (
-    <div className="row text-center">
-      { (!hasAuth ? ( 
-            <div className="row text-center">
-              <div className="col-12 offset-2">
-                <form className="my-1 py-1 mx-5 px-5 w-50" onSubmit={handleLogin}>
-                  <h4>Login</h4>
-                  <label htmlFor="newuser">Usuario</label>
-                  <input id="newuser"className="form-control" placeholder="Usuario" onChange={(e) => setForm({ ...form, username: e.target.value })} />
-                  <label htmlFor="newpassword">Contraseña</label>
-                  <input id="newpassword" className="form-control" type="password" placeholder="Contraseña" onChange={(e) => setForm({ ...form, password: e.target.value })} />
-                  <button className="btn btn-secondary my-2 w-100" type="submit" >Entrar</button>
-                  {error && <p style={{ color: "red" }}>{error}</p>}
-                </form>
+    <div className="container container-style bg-white rounded-4 w-50 shadow ">   
+        { (!hasAuth ? ( 
+              <div className="row justify-content-center text-center">
+                <div className="col-md-10 col-lg-10">
+                  <form className="my-1 py-1 px-4" onSubmit={handleLogin}>
+                    <i className="fa-solid fa-mug-hot display-6 my-3"></i>
+                    <h2>CoffeeConnect</h2>
+                    <div className="input-group my-4">
+                      <span className="input-group-text"><i className="fas fa-envelope"></i></span>
+                      <input id="newuser"className="form-control" placeholder="Usuario" onChange={(e) => setForm({ ...form, username: e.target.value })} />
+                    </div>
+                     <div className="input-group my-4">
+                        <span className="input-group-text"><i className="fas fa-lock"></i></span>
+                        <input id="newpassword" className="form-control" type={showPassword ? 'text' : 'password'} placeholder="Contraseña" onChange={(e) => setForm({ ...form, password: e.target.value })} />
+                        <button type="button" onClick={() => setShowPassword(!showPassword)}className="btn btn-link text-secondary position-absolute top-50 end-0 translate-middle-y me-2 p-0">
+                            <i className={`fas ${showPassword ? 'fa-eye-slash' : 'fa-eye'}`}></i>
+                        </button>
+                    </div>
+                    <button className="btn btn-warning my-2 w-100 text-white custom-brown" type="submit" >Iniciar sesion</button>
+                    {error && <p style={{ color: "red" }}>{error}</p>}
+                  </form>
+                   <p>¿No tienes cuenta? <a className="text-decoration-none link-warning" href="">Regístrate</a> </p> 
+                </div>
               </div>
+            ) : (  
+              <div className="col-12">
+                <div className="text-center spinner-border text-primary" role="status">
+                </div> 
+                <button className="btn btn-secondary my-1 w-100" onClick={handleLogout}>Logout</button> 
+                <p>sesion iniciada</p>
             </div>
-          ) : (  
-            <div className="col-12">
-              <div className="text-center spinner-border text-primary" role="status">
-              </div> 
-              <button className="btn btn-secondary my-1 w-100" onClick={handleLogout}>Logout</button> 
-              <p>sesion iniciada</p>
-          </div>
-        ))
-      }
+          ))
+        }
+
     </div>
   );
 }
